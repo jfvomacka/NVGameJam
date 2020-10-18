b8702f8fa36e0923926bf779b1b951a0373bc9c8
Omar Uraimov <uraimov@usc.edu>
2020-10-18 14:29:06 -0700

    more menu

---
+++ Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game
+{
+    public class InGameMenu : MonoBehaviour
+    {
+
+        public void PlayAgain()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
